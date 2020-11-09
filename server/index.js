7ac111328e57045b1464ecf6bb17ec4531634354
mpottebaum <mpottebaum@gmail.com>
2020-11-08 21:36:50 -0600

    removed react-scripts + configured webpack, node server

---
+++ server/index.js
@@ -0,0 +1,50 @@
+/* eslint consistent-return:0 import/order:0 */
+
+const express = require('express');
+const logger = require('./logger');
+
+const argv = require('./argv');
+const port = require('./port');
+const setup = require('./middlewares/frontendMiddleware');
+const isDev = process.env.NODE_ENV !== 'production';
+const ngrok =
+  (isDev && process.env.ENABLE_TUNNEL) || argv.tunnel
+    ? require('ngrok')
+    : false;
+const { resolve } = require('path');
+const app = express();
+
+setup(app, {
+  outputPath: resolve(process.cwd(), 'public'),
+  publicPath: '/',
+});
+
+app.set('port', process.env.PORT || 3001)
+
+const customHost = argv.host || process.env.HOST;
+const host = customHost || null;
+const prettyHost = customHost || 'localhost';
+
+app.get('*.js', (req, res, next) => {
+  req.url = req.url + '.gz'; // eslint-disable-line
+  res.set('Content-Encoding', 'gzip');
+  next();
+});
+
+app.listen(port, host, async err => {
+  if (err) {
+    return logger.error(err.message);
+  }
+
+  if (ngrok) {
+    let url;
+    try {
+      url = await ngrok.connect(port);
+    } catch (e) {
+      return logger.error(e);
+    }
+    logger.appStarted(port, prettyHost, url);
+  } else {
+    logger.appStarted(port, prettyHost);
+  }
+});
